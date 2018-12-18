6dac35ac3b98fc668e09ad6b9c92eda8b68dd29f
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2018-12-17 23:30:46 -0300

    Teste método remove

--- src/pilha/TestaPilha.java
+++ src/pilha/TestaPilha.java
@@ -11,6 +11,16 @@ public class TestaPilha {
 		pilha.insere("Teti");
 		System.out.println(pilha);
 		
+		pilha.insere("Dunga");
+		System.out.println(pilha);
+		
+		String r1 = pilha.remove();
+		
+		System.out.println(pilha);
+		String r2 = pilha.remove();
+		String r3 = pilha.remove();
+		
+		System.out.println(pilha);
 		
 	}
 
