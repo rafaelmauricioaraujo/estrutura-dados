f73b6504ce2ac01318dc6f87baf3b0f0175583c6
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2018-12-21 09:59:10 -0300

    Classe Queue

--- src/fila/TesteFila.java
+++ src/fila/TesteFila.java
@@ -1,5 +1,8 @@
 package fila;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TesteFila {
 	
 	public static void main(String[] args) {
@@ -9,6 +12,23 @@ public class TesteFila {
 		fila.adiciona("Teti");
 		
 		System.out.println(fila);
+		
+		String primeiro = fila.remove();
+		
+		System.out.println(primeiro);
+		
+		System.out.println(fila);
+		
+		//Java ja tem uma Fila
+		
+		Queue<String> filaDoJava = new LinkedList<String>();
+		filaDoJava.add("Dunga");
+		filaDoJava.add("Vivi");
+		System.out.println(filaDoJava);
+		
+		filaDoJava.poll();
+		System.out.println(filaDoJava);
+		
 	}
 	
 }
