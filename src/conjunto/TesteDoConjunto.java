bfa9610716f37596a0ca0648a8a70c15db716df4
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2018-12-21 19:31:05 -0300

    métodos para pegar o índice e método adiciona

--- src/conjunto/TesteDoConjunto.java
+++ src/conjunto/TesteDoConjunto.java
@@ -1,5 +1,17 @@
 package conjunto;
 
-public class TesteDoConjunto {
+import java.util.LinkedList;
 
+public class TesteDoConjunto {
+	
+	public static void main(String[] args) {
+		
+		Conjunto conjunto = new Conjunto();
+		
+		conjunto.adiciona("Alemao");
+		conjunto.adiciona("Dunga");
+		conjunto.adiciona("Amor");
+		
+		System.out.println(conjunto);
+	}
 }
