7a03807e79a643ce5acd3dccd06cb3717090556b
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2018-12-21 09:51:25 -0300

    Métodos Adiciona e Remove

--- src/fila/Fila.java
+++ src/fila/Fila.java
@@ -1,5 +1,21 @@
 package fila;
 
-public class Fila {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Fila {
+	private List<String> alunos = new LinkedList<String>();
+	
+	public void adiciona(String aluno) {
+		alunos.add(aluno);
+	}
+	
+	public String remove() {
+		return alunos.remove(0);
+	}
+	
+	@Override
+	public String toString() {
+		return alunos.toString();
+	}
 }
---
+++ src/fila/TesteFila.java
@@ -0,0 +1,14 @@
+package fila;
+
+public class TesteFila {
+	
+	public static void main(String[] args) {
+		Fila fila = new Fila();
+		
+		fila.adiciona("Rafael");
+		fila.adiciona("Teti");
+		
+		System.out.println(fila);
+	}
+	
+}
