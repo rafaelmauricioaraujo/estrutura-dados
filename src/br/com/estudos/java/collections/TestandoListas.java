991f01e5ae884e7f965cdac2d02f2ba4b6bcbd68
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-13 17:36:46 -0300

    Criando um ArrayList

--- src/br/com/estudos/java/collections/TestandoListas.java
+++ src/br/com/estudos/java/collections/TestandoListas.java
@@ -1,5 +1,16 @@
 package br.com.estudos.java.collections;
 
+import java.util.ArrayList;
+
 public class TestandoListas {
+	
+	public static void main(String[] args) {
+		
+		String aula1 = "Conhecendo mais de listas";
+		String aula2 = "Modelando a classe Aula";
+		String aula3 = "Trabalhando com Cursos e Sets";
+		
+		ArrayList<String> aulas = new ArrayList<>();
+	}
 
 }
