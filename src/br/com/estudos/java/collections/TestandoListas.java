2e539c05db5b79a15a5d762013c6aa8578f89578
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-13 17:46:24 -0300

    alguns métodos do ArrayList

--- src/br/com/estudos/java/collections/TestandoListas.java
+++ src/br/com/estudos/java/collections/TestandoListas.java
@@ -11,6 +11,21 @@ public class TestandoListas {
 		String aula3 = "Trabalhando com Cursos e Sets";
 		
 		ArrayList<String> aulas = new ArrayList<>();
+		
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+		
+		System.out.println(aulas);
+		
+		aulas.remove(0);
+		
+		System.out.println(aulas);
+		
+		for (String aula : aulas) {
+			System.out.println("Aula: " + aula);
+		}
+		
 	}
-
+	
 }
