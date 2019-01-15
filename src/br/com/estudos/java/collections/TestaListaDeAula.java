233bc6cb5d7138e97b9e467152550a8fcec69eed
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-14 23:23:25 -0300

    Criando a classe de testes da lista de aula

---
+++ src/br/com/estudos/java/collections/TestaListaDeAula.java
@@ -0,0 +1,19 @@
+package br.com.estudos.java.collections;
+
+import java.util.ArrayList;
+
+public class TestaListaDeAula {
+	public static void main(String[] args) {
+		Aula a1 = new Aula("Revisitando as ArrayList", 21);
+		Aula a2 = new Aula("Lista de objetos", 15);
+		Aula a3 = new Aula("Relacionamento de listas e objetos", 15);
+		
+		ArrayList<Aula> aulas = new ArrayList<>();
+		
+		aulas.add(a1);
+		aulas.add(a2);
+		aulas.add(a3);
+		
+		System.out.println(aulas);
+	}
+}
