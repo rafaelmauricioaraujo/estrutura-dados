0e15910cdcc4e1d17e64ab07b9b0ca35f52a0399
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-14 23:17:41 -0300

    Implementando classe Aula

---
+++ src/br/com/estudos/java/collections/Aula.java
@@ -0,0 +1,22 @@
+package br.com.estudos.java.collections;
+
+public class Aula {
+	
+	private String titulo;
+	private int tempo;
+	
+	public Aula(String titulo, int tempo) {
+		this.titulo = titulo;
+		this.tempo = tempo;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public int getTempo() {
+		return tempo;
+	}
+	
+
+}
