167ec67a5434161ac0035cf531d076841abc5b7f
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-19 15:32:51 -0300

    removendo imports desnecessários

--- src/br/com/estudos/java/collections/TestaCurso.java
+++ src/br/com/estudos/java/collections/TestaCurso.java
@@ -1,14 +1,14 @@
 package br.com.estudos.java.collections;
 
-import java.util.List;
-
 public class TestaCurso {
 	
 	public static void main(String[] args) {
 		
 		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
 		
-		javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleçoes", 22));
 		
 		System.out.println(javaColecoes.getAulas());
 	}
