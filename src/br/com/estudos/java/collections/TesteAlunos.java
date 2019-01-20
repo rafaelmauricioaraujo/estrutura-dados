00b2a1cfdbc731549ea9333e9fee5a70cd4f182a
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-19 22:32:53 -0300

    conhecendo o Collections.Set e HashSet

--- src/br/com/estudos/java/collections/TesteAlunos.java
+++ src/br/com/estudos/java/collections/TesteAlunos.java
@@ -1,8 +1,29 @@
 package br.com.estudos.java.collections;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TesteAlunos {
 	
 	public static void main(String[] args) {
+		
+		Set<String> alunos = new HashSet<>();
+		
+		alunos.add("Rafael Araujo");
+		alunos.add("Nathercia Barreto");
+		alunos.add("Dunga Doido");
+		alunos.add("Vivi Princesa");
+		alunos.add("Gigi Surtada");
+		
+		System.out.println(alunos);
+		
+		for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+		
+		alunos.forEach(aluno -> {
+			System.out.println("aluno:" + aluno);
+		});
 		
 	}
 
