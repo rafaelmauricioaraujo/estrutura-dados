95935bb02fd7f3a9135f58e15ee08704d55e4021
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-20 21:09:46 -0300

    construtores e getters

--- src/br/com/estudos/java/collections/Aluno.java
+++ src/br/com/estudos/java/collections/Aluno.java
@@ -1,6 +1,22 @@
 package br.com.estudos.java.collections;
 
 public class Aluno {
+
+	private String nome;
+	private int numeroMatricula;
+
+	public Aluno(String nome, int numeroMatricula) {
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getNumeroMatricula() {
+		return numeroMatricula;
+	}
 	
 	
 
