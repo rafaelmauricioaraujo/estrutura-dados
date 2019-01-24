ecdf42417987a867616dbfb9761ff2e9ee59b234
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-24 18:30:38 -0300

    TreeSet precisa de um comparetor que pode ser passado no construtor

--- src/br/com/estudos/java/collections/TesteRecibo.java
+++ src/br/com/estudos/java/collections/TesteRecibo.java
@@ -1,5 +1,14 @@
 package br.com.estudos.java.collections;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class TesteRecibo {
+	
+	Set<Recibo> recibos = new TreeSet<>();
+	
+	Recibo r01 = new Recibo(01, "Qualquer");
+	Recibo r02 = new Recibo(02, "OutroQualquer");
+	Recibo r03 = new Recibo(03, "Seil·Meu");
 
 }
